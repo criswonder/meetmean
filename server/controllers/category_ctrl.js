07ebedabec2caf9b28e48be8e0d57c18b734e2f5
andymao <wb-maohongyun@alibaba-inc.com>
2014-08-12 10:31:06 +0800

    fix some rend bug

--- server/controllers/category_ctrl.js
+++ server/controllers/category_ctrl.js
@@ -187,7 +187,7 @@ exports.create_ablum_item = function(req, res) {
 
                 return res.status(400);
             }else{
-                res.send({redirect:true}).redirect(200,'albums');    
+                return res.redirect(200,'albums');    
             }
         
     });
